7b2865ad056fb479a92a0b0cc3343cb6b86f6c4b
CesarIzk <125629498+CesarIzk@users.noreply.github.com>
2025-09-09 22:55:04 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("triviaBtn").addEventListener("click", () => {
+  alert("Aquí va la Trivia 🎯");
+});
+
+document.getElementById("historiaBtn").addEventListener("click", () => {
+  alert("Aquí va la Historia 📖");
+});
+
+document.getElementById("manualBtn").addEventListener("click", () => {
+  window.open("manual.html", "_blank"); // Abre en una nueva ventana/pestaña
+});
+
+document.getElementById("statsBtn").addEventListener("click", () => {
+  alert("Aquí van las Estadísticas 📊");
+});
